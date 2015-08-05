cbee1885c6c0a9e01819571278b5e23e596f99cd
kyleYang <yangzychina@gmail.com>
2015-08-05 16:51:52 +0800

    Initial Commit

---
+++ iOutdoors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOutdoors
+//
+//  Created by Kyle on 15/8/5.
+//  Copyright (c) 2015年 xiaoluuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
